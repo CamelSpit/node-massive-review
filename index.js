805509f9021c8b135a3051eaee77e47825c3ac30
meganmfisher <meganmichellefisher@gmail.com>
2017-12-18 09:04:37 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Massive is a PostgreSQL-specific data access tool. The goal of Massive is to make it easier for you to use PostgreSQL's amazing features. It helps you access and query your database as you want to.
+
+//You can create a directory in the root of your project called "db" and Massive will load the SQL files therein and make them executable based on file name.
+
+const express = require('express')
+, cors = require('cors')
+, bodyParser = require('body-parser')
+, massive = require('massive')
+, controller = require('./products_controller')
+require('dotenv').config()
+
+
+
+//express()
+// Creates an Express application. 
+const app = express();
+
+//app.use()
+// Mounts the specified middleware function or functions at the specified path: the middleware function is executed when the base of the requested path matches path.
+
+//bodyParser.json() returns middleware that only parses json. A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body).
+app.use(bodyParser.json());
+
+
+// Cross-Origin Resource Sharing (CORS) is a mechanism that uses additional HTTP headers to let a user agent gain permission to access selected resources from a server on a different origin (domain) than the site currently in use. A user agent makes a cross-origin HTTP request when it requests a resource from a different domain, protocol, or port than the one from which the current document originated. cors() allows you to make those cross-origin HTTP requests.
+app.use(cors());
+
+
+//connect to the database. This will load the tables and functions, returning them to the db instance
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+    // app.set(name, value)
+    // Assigns setting name to value. You may store any value that you want, but certain names can be used to configure the behavior of the server. 'db' is the name. dbInstance is the value.
+app.set('db', dbInstance);
+});
+
+
+//app.get(path, callback)
+// Routes HTTP GET requests to the specified path with the specified callback function.
+app.get('/api/products', controller.getAll)
+
+//app.get(path, callback)
+// Routes HTTP GET requests to the specified path with the specified callback function.
+app.get('/api/product/:id', controller.getOne)
+
+//app.put(path, callback)
+// Routes HTTP PUT requests to the specified path with the specified callback function.
+app.put('/api/product/:id', controller.update)
+
+// app.post(path, callback)
+// Routes HTTP POST requests to the specified path with the specified callback function.
+app.post('/api/product/', controller.create)
+
+// app.delete(path, callback)
+// Routes HTTP DELETE requests to the specified path with the specified callback function. 
+app.delete('/api/product/:id', controller.delete)
+
+
+
+
+const port = process.env.PORT || 3000;
+
+// app.listen()
+// Listens for connections on the specified port. 
+app.listen(port, () => console.log(`Listening on port ${port}`));
